bfed81346a0993f1c51a5064c7aa659f1a930908
Prashu <1707prash@gmail.com>
2021-06-27 12:46:31 +0530

    ay 10

---
+++ Day 10/script.js
@@ -0,0 +1,96 @@
+const game = document.querySelector(".game");
+
+for(let i=0; i<12; i++){
+    game.innerHTML += "<div class='card'></div>";
+}
+
+let arr = [
+    'https://i1.sndcdn.com/artworks-000137862127-onf2mn-t500x500.jpg',
+    'https://i1.sndcdn.com/artworks-5BM7n5ycdbSpks9d-aAm7VQ-t500x500.jpg', 
+    'https://i1.sndcdn.com/artworks-wcoaBr6M7ZeYuQMi-V5CYXw-t500x500.jpg',
+    'https://i.pinimg.com/originals/78/f0/ba/78f0bae24686b1ee48c83d466de80a5a.png',
+    'https://i1.sndcdn.com/artworks-000059018954-5rr08r-t500x500.jpg',
+    'https://i1.sndcdn.com/avatars-dARAyX420aHF5tph-QnLKxg-t500x500.jpg'
+];
+let content = [...arr,...arr];
+let urls= [];
+for(let i=0; i<12; i++){
+    let rand = Math.floor(Math.random()*(12-i));
+    urls.push(`url(${content[rand]})`);
+    content.splice(rand, 1);
+}
+
+const cards = document.querySelectorAll(".card");
+const points = document.querySelector(".points");
+let purl='';
+let pi1=-1, pi2=-2;
+let count =0;
+let moves =0;
+
+function foundCard(){
+    cards[pi1].classList.add("cardFound");
+    cards[pi2].classList.add("cardFound");
+}
+
+function cardClicked(i){  
+    cards[i].style.transition = "all 300ms ease-in";
+    cards[i].style.backgroundImage = urls[i];
+    cards[i].style.backgroundSize = "150px";
+    cards[i].style.transform ="rotateY( 180deg )";
+}
+
+function cardBreak(){
+    cards[pi1].style.background="#D3D0CB";
+    cards[pi2].style.background="#D3D0CB";
+    cards[pi1].style.transform ="rotateY( 0deg )";
+    cards[pi2].style.transform ="rotateY( 0deg )";
+}
+
+for(let i=0; i<12; i++){
+    cards[i].addEventListener('click', ()=>{
+        moves++;
+        points.innerHTML = `${moves}`;
+        count++;
+        if(count===3){
+            count=1;
+            purl="";
+            cards[pi1].style.background="#D3D0CB";
+            cards[pi2].style.background="#D3D0CB";
+            cards[pi1].style.transform ="rotateY( 0deg )";
+            cards[pi2].style.transform ="rotateY( 0deg )";
+        }
+        if(count===1){
+            pi1=i;
+        }
+        if(count===2){
+            pi2=i;
+            setTimeout(cardBreak, 3000);
+        }
+        if(pi1!==pi2){
+                cardClicked(i);
+                if(purl===urls[i]){
+                    setTimeout(foundCard, 400);
+                }
+                else{
+                    purl = urls[i];
+                }  
+            
+        }
+    });
+}
+ 
+
+
+
+// const cardsClicked = document.querySelectorAll(".card-clicked");
+// for(let i=0; i<12; i++){
+//     cardsClicked[i].addEventListener('click', ()=>{
+//         // if(cards[i].classList.contains("card-clicked"))
+//         // {
+//             cardsClicked[i].style.animation= "card-rotate-back 2s ease-in";
+//         // }
+//         // else{
+//         //     cards[i].style.animation= "none";
+//         // }
+//     });
+// }
