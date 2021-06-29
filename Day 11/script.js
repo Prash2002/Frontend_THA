92427824bd1dc6accfdfb28a3166aadade7259fe
Prashu <1707prash@gmail.com>
2021-06-29 18:33:20 +0530

    homepage

---
+++ Day 11/script.js
@@ -0,0 +1,38 @@
+let question_set=[
+    {
+        question: "Sin City's cold and empty, no one's around to _____ me.",
+        option: ["dump","judge", "love", "hug"],
+        answer: 1
+    },
+    {
+        question: "Sin City's cold and empty, no one's around to _____ me.",
+        option: ["dump","judge", "love", "hug"],
+        answer: 1
+    },
+    {
+        question: "\"Sin City's cold and empty, no one's around to _____ me.\" - The Weeknd, 'Blinding Lights'",
+        option: ["dump","judge", "love", "hug"],
+        answer: 1
+    },
+    {
+        question: "Sin City's cold and empty, no one's around to _____ me.",
+        option: ["dump","judge", "love", "hug"],
+        answer: 1
+    },
+    {
+        question: "Sin City's cold and empty, no one's around to _____ me.",
+        option: ["dump","judge", "love", "hug"],
+        answer: 1
+    },
+]
+
+// document
+
+/*
+    <div class="qno">1.</div>
+            <div class="question">"If you win, you live, if you lose, you die. If you don't fight, you can't win."</div>
+            <button class="option">Naruto</button>
+            <button class="option">Tokyo Ghoul</button>
+            <button class="option">Attack on Titan</button>
+            <button class="option">One piece</button>
+*/
