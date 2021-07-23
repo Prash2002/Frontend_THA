54023c1761a10b33062fa36bc14ea4bd0e669114
Prasanthi G <1707prash@gmail.com>
2021-07-23 16:53:54 +0530

    build

---
+++ day-20/src/Items.js
@@ -0,0 +1,27 @@
+[
+    {
+        title:"Pizza",
+        cals: 265,
+        img: 'https://pbs.twimg.com/media/BuRQ-baCIAAoL1w.jpg',
+    },
+    {
+        title:"Burger",
+        cals: 295,
+        img: 'https://economictimes.indiatimes.com/thumb/msid-72863458,width-1200,height-900,resizemode-4,imgsize-731189/burger-thnkstck.jpg',
+    },
+    {
+        title:"Coke",
+        cals: 38,
+        img: "https://weburbanist.com/wp-content/uploads/2013/07/ice-coke-bottle-design.jpg",
+    },
+    {
+        title:"Brownie",
+        cals: 466,
+        img: "https://i2.wp.com/chefsavvy.com/wp-content/uploads/super-chewy-brownies.jpg",
+    },
+    {
+        title:"Lasagna",
+        cals: 135,
+        img:"https://www.lecremedelacrumb.com/wp-content/uploads/2020/02/make-ahead-lasagna-2sm-4.jpg",
+    },
+]
---
+++ day-20/src/components/Calory.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Calory.css';
+
+
+const Calory = (props) => {
+    var color = "rgb(6, 214, 160)";
+    var c = "rgb(7, 59, 76)"
+    if(props.cals> 250){
+        color= "rgb(239, 71, 111)";
+        c= "white"
+    }
+    
+    function deleteItem(){
+        console.log("oho")
+    }
+
+    return (
+        <div className="container" style={{backgroundColor: color, color: c}}>
+            <div className="item">{props.title}</div>
+            <button onClick={deleteItem}>delete</button>
+            <img src={props.img} alt="can't load" />
+            <div className="cal">Contains {props.cals} cals</div>
+        </div>
+    )
+}
+
+export default Calory
