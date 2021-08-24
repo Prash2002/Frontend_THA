80d44e06b10fa99671e92764fcffcd5269980c21
Prasanthi G <1707prash@gmail.com>
2021-08-24 15:56:06 +0530

    day 24 debugged

--- day-24/src/App.js
+++ day-24/src/App.js
@@ -1,27 +1,42 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
-import About from './components/About';
-import Dashboard from './components/Dashboard';
-import Home from './components/Home';
-import Nav from './components/Nav';
-import Profile from './components/Profile';
-import LoginContext from './context/LoginContext';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+import LoginContext from "./context/LoginContext";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
-          <LoginContext>
-        <Nav />
-        
-        <Switch>
-            <Route exact path='/' component={Home}></Route>
-            <Route exact path='/about' component={About}></Route>
-            <Route exact path='/profile' component={Profile}></Route>
-            <Route exact path='/dashboard' component={Dashboard}></Route>
-        </Switch>
-          </LoginContext>          
+        <LoginContext>
+          <Nav />
+
+          <Switch>
+            <Route
+              exact
+              path="/Frontend_THA/day-24/build/"
+              component={Home}
+            ></Route>
+            <Route
+              exact
+              path="/Frontend_THA/day-24/build/about"
+              component={About}
+            ></Route>
+            <Route
+              exact
+              path="/Frontend_THA/day-24/build/profile"
+              component={Profile}
+            ></Route>
+            <Route
+              exact
+              path="/Frontend_THA/day-24/build/dashboard"
+              component={Dashboard}
+            ></Route>
+          </Switch>
+        </LoginContext>
       </Router>
     </div>
   );
--- day-24/src/components/Nav.jsx
+++ day-24/src/components/Nav.jsx
@@ -1,36 +1,36 @@
-import {Link} from "react-router-dom";
-import React, {useContext} from 'react'
+import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import { Context } from "../context/LoginContext";
 
 const Nav = () => {
-    const {state} = useContext(Context);
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                
-                <li>
-                    {
-                        state.loggedin? 
-                            <Link to="/profile"> Profile</Link> : 
-                            <Link to="/"> Profile</Link>
-                    }
-                </li>
-                <li>
-                    {
-                        state.loggedin? 
-                            <Link to="/dashboard">Dashboard</Link>: 
-                            <Link to="/"> Dashboard</Link>
-                    }
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  const { state } = useContext(Context);
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/Frontend_THA/day-24/build/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Frontend_THA/day-24/build/about">About</Link>
+        </li>
 
-export default Nav
+        <li>
+          {state.loggedin ? (
+            <Link to="/Frontend_THA/day-24/build/profile"> Profile</Link>
+          ) : (
+            <Link to="/Frontend_THA/day-24/build/"> Profile</Link>
+          )}
+        </li>
+        <li>
+          {state.loggedin ? (
+            <Link to="/Frontend_THA/day-24/build/dashboard">Dashboard</Link>
+          ) : (
+            <Link to="/Frontend_THA/day-24/build/"> Dashboard</Link>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
